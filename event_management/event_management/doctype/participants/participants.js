53394180c3d5f8e6a5956b36613e10c06da4af83
aswar-tan <aswar@tandigital.id>
2024-06-19 12:52:32 +0800

    core[add doctype]

---
+++ event_management/event_management/doctype/participants/participants.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, konoha and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Participants", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ event_management/event_management/doctype/participants/test_participants.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, konoha and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestParticipants(FrappeTestCase):
+	pass
